35aa9b98abedea628ef8a35375a0aaab93e738a8
Abigail Tafa <abigailmutuvira@gmail.com>
2022-09-08 04:09:14 -0700

    a script that compiles a C file but does not link

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,16 @@
-#!/bin/bash
+
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
- * Return 0 after printing this function*/
+ * Return 0 after printing this function
+ * */
+
+
 int main (void)
-{puts ("\"Programming is like building a multilingual puzzle");
+
+{ 
+	puts("Programming is like building a multilingual puzzle"\");
 	return (0);
 }

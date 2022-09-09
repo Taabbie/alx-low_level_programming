452847c74dc0820cff994f9a47bf5982e244e3b7
Abigail Tafa <abigailmutuvira@gmail.com>
2022-09-09 14:15:12 -0700

    a program that will determine the last digit of a random number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,25 +3,25 @@
 #include <stdio.h>
 
 /**
- *  main -Entry point
+ *  main -program will print the last digit of a random number
  *
  *  Return: Always 0 (success/correct)
  */
 
 int main(void)
 {
-if (n % 10 > 5)
+int n
+printf("Last digit of %d is %d", n, n % 10);
+if(n % 10 > 5)
 {
-	printf("Last digit of %i is %i and is greater than 5\n", n, n % 10);
+printf("and is greater than 5\n");
 }
-else if (n % 10 == 0)
+else if(n % 10 == 0)
 {
-	printf("Last digit of %i is %i and is 0\n", n, n % 10);
-else 
+printf("and is 0\n")
+}
+else if(n % 10 < 6 && n % 10 != 0)
 {
-		printf("Last digit of %i is less than 6 and not 0\n", n, n % 10);
-}
-}
-
-return (0);
+printf("and is less than 6 and not 0\n"); 
+return(0);
 }

b298ca18c4bc3fe4aeaa68e1c24b863819544fed
Taabbie <111085482+Taabbie@users.noreply.github.com>
2022-09-09 21:13:20 +0200

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,10 +17,10 @@ if (n % 10 > 5)
 else if (n % 10 == 0)
 {
 	printf("Last digit of %i is %i and is 0\n", n, n % 10);
+}
 else 
 {
 		printf("Last digit of %i is less than 6 and not 0\n", n, n % 10);
-}
 }
 
 return (0);

2697444f9523f9c1b41c6067e626540c04a49360
Abigail Tafa <abigailmutuvira@gmail.com>
2022-09-15 03:13:39 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isdigit - function to check for a digit
+ * @c: integer argument
+ * Return : Something Success
+ */
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}

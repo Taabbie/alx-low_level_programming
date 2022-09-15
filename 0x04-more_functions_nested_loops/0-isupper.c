74e4925c97bf6c23db73953c2b525a42948e3f84
Abigail Tafa <abigailmutuvira@gmail.com>
2022-09-15 06:56:18 -0700

    Hot fix

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,16 +3,9 @@
 /**
  * _isupper - checks for upper case in letters
  * @c: integer to be checked
- * Return: 1 for upper or 0 for any other case
+ * Return: something but success
  */
 int _isupper(int c)
 {
-	if (c >= 65 && c <= 90)
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+return (c >= 'A' && c <= 'Z');
 }

4e0dd32ee27204ae9ebcc9ed661c71a6837dc028
Abigail Tafa <abigailmutuvira@gmail.com>
2022-09-19 10:27:17 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string
+ * @s: the string to be reversed
+ */
+void rev_string(char *s)
+{
+	char temp;
+	int i, len, len1;
+
+	len = 0;
+	len1 = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	for (i = 0; i < len / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[len1];
+		s[len1] = temp;
+	}
+}

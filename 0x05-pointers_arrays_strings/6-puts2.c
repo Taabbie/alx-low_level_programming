4c62444072b6660766b820570e29a3d1567b53c1
Abigail Tafa <abigailmutuvira@gmail.com>
2022-09-19 10:45:03 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts2 - prints one character out of two
+ * of a string followed by a new line
+ * @str: the string to print the characters from
+ */
+void puts2(char *str)
+{
+	int len, i;
+
+	len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	for (i = 0; i < len; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}

8889eeda48a3ef1b2fec2e4d300f0abb1ad0c73f
Abigail Tafa <abigailmutuvira@gmail.com>
2022-09-22 01:39:29 -0700

    hot fix

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,19 +1,33 @@
 #include "main.h"
 
 /**
- * reverse_array - reverses the contents of an array
- * @a: array
- * @n: number of elements
+ *cap_string - capitalizes every word of a string
+ * @s: string to modify
+ *
+ *Return: the capitalized string
  */
-
-void reverse_array(int *a, int n)
+char *cap_string(char *s)
 {
-	int temp, index;
+	int i, j;
 
-	for (index = n - 1; index > n / 2; index--)
+	char spe[13] = {',', '\t', '\n', ',', ';', '.',
+	'!', '?', '"', '(', ')', '{', '}'};
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		temp = a[n - 1 - index];
-		a[n - 1 - index] = a[index];
-		a[index] = temp;
+		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
+
+		for (j = 0; j < 13; j++)
+		{
+			if (s[i] == spe[j])
+			{
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				{
+					s[i + 1] -= 32;
+				}
+			}
+		}
 	}
+	return (s);
 }

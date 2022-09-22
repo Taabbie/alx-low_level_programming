56cec91efde5e533455d00b991a2b998d73c5f2c
Abigail Tafa <abigailmutuvira@gmail.com>
2022-09-22 00:07:43 -0700

    hot fix

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,12 +13,12 @@ int _strcmp(char *s1, char *s2)
 {
 	while (*s1 == *s2)
 	{
-		if (s1[1] == '\0')
+		if (*s1 == '\0')
 		{
 			return (0);
 		}
 		s1++;
 		s2++;
 	}
-	return (diff);
+	return (*s1 - *s2);
 }

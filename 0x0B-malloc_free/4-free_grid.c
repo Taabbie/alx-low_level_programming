712ed7ba741428a4cf444c61c36ae3aa9f41f52e
Abigail Tafa <abigailmutuvira@gmail.com>
2022-10-05 02:36:11 -0700

    4-free_grid.c

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * free_grid -frees a 20 grid creates in alloc_grid
+ * @grid: ponter to pointer
+ * @height: height of the grid
+ * Return: void
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}

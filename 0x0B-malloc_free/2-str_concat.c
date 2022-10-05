652ff05d52c741750089248e0979efdba37231d0
Abigail Tafa <abigailmutuvira@gmail.com>
2022-10-04 23:46:28 -0700

    hot fix

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,31 +13,41 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *s3;
-	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
+	unsigned int len1, len2, size, i, j;
+	char *nstr;
 
-	while (s1 && s1[len1])
+	if (s1 == NULL)
+		s1--;
+	if (s2 == NULL)
+		s2--;
+
+	len1 = 0;
+	while (s1[len1] != '\0')
 		len1++;
-	while (s2 && s2[len2])
+	len2 = 0;
+	while (s2[len2] != '\0')
 		len2++;
 
-	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
-	if (s3 == NULL)
+	size = len1 + len2;
+
+	nstr = malloc((sizeof(char) * size) + 1);
+	/* check if malloc was successful*/
+	if (nstr == NULL)
 		return (NULL);
 
-	i = 0;
-	j = 0;
-
-	if (s1)
+	1 = 0;
+	while (i < len1)
 	{
-		while (i < (len1 + len2))
-		{
-			s3[i] = s2[j];
-			i++;
-			j++;
-		}
+		nstr[i] = s1[i];
+		i++;
 	}
-	s3[i] = '\0';
-
-	return (s3);
+	j = 0;
+	while (i <= size)
+	{
+		nstr[i] = s2[j];
+		i++;
+		j++;
+	}
+	return (nstr);
 }
+
